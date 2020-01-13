0692eaebc3c949990f6116ed89b762943d2293c5
pulawatterachusong <59813889+pulawatterachusong@users.noreply.github.com>
2020-01-13 10:47:44 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World 1 \n");
+    printf("Hello World 2 \n");
+    printf("Hello World 3 \n");
+    printf("Hello World 5 \n");
+    printf("Hello World 6 \n");
+    printf("Hello World 7 \n");
+    printf("Hello World 8 \n");
+    printf("Hello World 9 \n");
+    printf("Hello World 10 \n");
+    printf("Hello World 11 \n");
+    printf("Hello World 12 \n");
+    printf("Hello World 13 \n");
+    printf("Hello World 14 \n");
+    printf("Hello World 15 \n");
+    printf("Hello World 16 \n");
+    printf("Hello World 17 \n");
+    printf("Hello World 18 \n");
+    printf("Hello World 19 \n");
+    printf("Hello World 20 \n");
+    
+    
+
+    return 0;
+}
